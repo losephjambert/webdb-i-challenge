bd37316c6dd8cfa919528a062fcf6588c60b0215
Joe Lambert <josephc.lambert@gmail.com>
2019-11-04 20:23:36 -0800

    feature: adds GET request for accounts

--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
-const db = require('../data/dbConfig.js');
+const knex = require('../data/dbConfig.js');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.status(200).json({ message: `${req.method}: Hello from the accounts resource 👋` });
+  try {
+    const accounts = await knex.select('*').from('accounts');
+    res.status(200).json(accounts);
+  } catch (error) {
+    res.status(500).json({ message: `Error retrieving accounts from the database` });
+  }
 });
 
 router.post('/', async (req, res) => {
