97270d529a2aaf0cd7e328026edd586489141542
Joe Lambert <josephc.lambert@gmail.com>
2019-11-04 20:55:09 -0800

    feature: POST new account

--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -14,7 +14,38 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  res.status(200).json({ message: `${req.method}: Hello from the accounts resource 👋` });
+  const { name, budget } = req.body;
+  if (!name) {
+    res.status(400).json({ message: `Account name required` });
+  } else if (!budget) {
+    res.status(400).json({ message: `Account budget required` });
+  } else if (typeof budget !== 'number') {
+    res.status(400).json({ message: `Account budget must be a number` });
+  } else {
+    try {
+      const accountExists = await (await knex.select('name').from('accounts')).find(
+        n => n.name === name
+      );
+      if (accountExists) {
+        res.status(400).json({ message: `Account with name ${name} already exists.` });
+      } else {
+        try {
+          const newAccount = await knex.insert({ name, budget }, 'id').into('accounts');
+          const account = await knex
+            .select('*')
+            .from('accounts')
+            .where('id', '=', newAccount[0])
+            .first();
+          console.log(newAccount[0], account);
+          res.status(200).json(account);
+        } catch (error) {
+          res.status(500).json({ message: `Error retrieving accounts from the database` });
+        }
+      }
+    } catch (error) {
+      res.status(500).json({ message: `Error validating unique name of account` });
+    }
+  }
 });
 
 router.put('/:id', async (req, res) => {
