bbae4bf6c86cc75fd9ba946017db70748af8b76d
Joe Lambert <josephc.lambert@gmail.com>
2019-11-04 16:09:06 -0800

    feature: scaffolds CRUD ops

--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -5,7 +5,19 @@ const db = require('../data/dbConfig.js');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.status(200).json({ message: `Hello from the accounts resource 👋` });
+  res.status(200).json({ message: `${req.method}: Hello from the accounts resource 👋` });
+});
+
+router.post('/', async (req, res) => {
+  res.status(200).json({ message: `${req.method}: Hello from the accounts resource 👋` });
+});
+
+router.put('/:id', async (req, res) => {
+  res.status(200).json({ message: `${req.method}: Hello from the accounts resource 👋` });
+});
+
+router.delete('/:id', async (req, res) => {
+  res.status(200).json({ message: `${req.method}: Hello from the accounts resource 👋` });
 });
 
 module.exports = router;
